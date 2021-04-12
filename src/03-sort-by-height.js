3391f1a7a6c36ac89b0635f74375c94c08332692
Vitali <terrmit574@gmail.com>
2021-04-12 19:55:39 +0300

    feat: resolve 03-sort-by-height

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,21 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const newArr = [];
+  arr.forEach((element) => {
+    if (element !== -1) {
+      newArr.push(element);
+    }
+  });
+  newArr.sort((a, b) => a - b);
+  const result = arr.map((element) => {
+    if (element !== -1) {
+      return Number(newArr.splice(0, 1));
+    }
+    return element;
+  });
+  return result;
 }
 
 module.exports = sortByHeight;
